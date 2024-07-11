5104005b78c80fbfd3588f823be6401c650eda4e
AlexKostjuk <alkost198333@gmail.com>
2024-07-11 13:10:00 +0300

    dz_15 + model_start1

--- parcel/models.py
+++ parcel/models.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from post_machin.models import PostMachin
+
+
 # Create your models here.
 class Parcel(models.Model):
     recipient = models.ForeignKey(User)
     sender = models.ForeignKey(User)
     size =models.ImageField()
-    post_machin = models.ForeignKey(PostMacine)
+    post_machin = models.ForeignKey(PostMachin)
     order_date_time = models.DateTimeField('date published')
     open_date_time = models.DateTimeField('date published')
-    status = models.BooleanField(default=0)
+    status = models.BooleanField(default=False)
 
 
 
--- post_machin/models.py
+++ post_machin/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class PostMachin(models.Model):
+    adress = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+
+
+class Locker(models.Model):
+    size = models.ImageField()
+    post_machin = models.ForeignKey(PostMachin)
+    status = models.BooleanField(default=False)
+
+
+
