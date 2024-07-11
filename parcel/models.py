cc967d3722d15e6376fb7139a6578dfbb9be51a7
AlexKostjuk <alkost198333@gmail.com>
2024-07-11 13:20:39 +0300

    dz_15 + model_start2

--- parcel/models.py
+++ parcel/models.py
@@ -7,9 +7,9 @@ from post_machin.models import PostMachin
 # Create your models here.
 class Parcel(models.Model):
     recipient = models.ForeignKey(User, on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
-    size =models.ImageField()
-    post_machin = models.ForeignKey(PostMachin)
+    sender = models.CharField(max_length=200)
+    size = models.ImageField()
+    post_machin = models.ForeignKey(PostMachin, on_delete=models.CASCADE)
     order_date_time = models.DateTimeField('date published')
     open_date_time = models.DateTimeField('date published')
     status = models.BooleanField(default=False)
