5042d0e013982ea6cce472f526e1864c6f179f71
AlexKostjuk <alkost198333@gmail.com>
2024-07-11 13:00:03 +0300

    dz_15

--- parcel/models.py
+++ parcel/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Parcel(models.Model):
+    recipient = models.ForeignKey(User)
+    sender = models.ForeignKey(User)
+    size =models.ImageField()
+    post_machin = models.ForeignKey(PostMacine)
+    order_date_time = models.DateTimeField('date published')
+    open_date_time = models.DateTimeField('date published')
+    status = models.BooleanField(default=0)
+
