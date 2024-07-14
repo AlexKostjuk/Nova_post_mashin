7fb4acc36ce1ace6142666169664fd118298329e
AlexKostjuk <alkost198333@gmail.com>
2024-07-14 10:48:28 +0300

    dz_16+user

--- Nova_post_mashin/urls.py
+++ Nova_post_mashin/urls.py
@@ -23,6 +23,10 @@ import user.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', user.views.user_page),
+    path('login/', user.views.login_view),
+    path('logout/', user.views.logout_view),
+    path('register/', user.views.register_view),
+
     path('parcel/', include('parcel.urls')),
     path('post_machin/', include('post_machin.urls')),
 
--- user/views.py
+++ user/views.py
@@ -1,6 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def login_view (request):
+   context = {}
+   if request.method == 'POST':
+      username = request.POST['username']
+      passsword = request.POST['password']
+      user = authenticate(username=username, passsword=passsword)
+      if user is not None:
+         login(request, user)
+         return redirect('/user/')
+      else:
+         context['error'] = 'invalid username or pasword'
+   return render(request, 'login.html', context=context)
+
+
+def logout_view(request):
+   logout(request)
+   return redirect('/login/')
+
+
+def register_view(request):
+   if request.method == 'POST':
+      username = request.POST['username']
+      passsword = request.POST['password']
+      email = request.POST['email']
+      fname = request.POST['fname']
+      lname = request.POST['lname']
+      user = User.objects.create_user(username=username, email=email, passsword=passsword, fname=fname, lname=lname)
+      user.save()
+      return redirect('/login/')
+   else:
+      return render(request, 'register.html')
+
+
+
 def user_page (request):
    return HttpResponse("helo start")
