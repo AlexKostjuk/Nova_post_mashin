51ca9db3aa8a2a3f394320c655d39a1cd813e2c0
AlexKostjuk <alkost198333@gmail.com>
2024-07-14 17:45:42 +0300

    dz_17 + form_login

--- parcel/views.py
+++ parcel/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 # Create your views here.
 from django.shortcuts import render
-from parcel import models
+from parcel import models, forms
+
 
 # Create your views here.
 def parcels_view(request):
@@ -13,3 +14,20 @@ def parcels_view(request):
 def one_parcel_view(request, parcel_id):
     result = models.Parcel.objects.get(pk=parcel_id)
     return render(request, 'one_parcel.html', context={'parcel': result})
+
+
+# def new_parcel(request):
+#     if request.method == 'POST':
+#         form = forms.ParcelForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('form.save()')
+#         else:
+#             return HttpResponse('not form.save()')
+#     else:
+#         render(request, 'qwer.html', context=forms.ParcelForm())
+
+
+
+
+
--- user/views.py
+++ user/views.py
@@ -4,19 +4,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
+from user.forms import LoginForm
+
+
 # Create your views here.
 
 def login_view (request):
    context = {}
    if request.method == 'POST':
-      username = request.POST['username']
-      password = request.POST['password']
-      user = authenticate(username=username, password=password)
-      if user is not None:
-         login(request, user)
-         return redirect('/user/')
-      else:
-         context['error'] = 'invalid username or pasword'
+      form = LoginForm(request.POST)
+      if form.is_valid():
+         user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+         if user is not None:
+            login(request, user)
+            return redirect('/user/')
+      context['error'] = 'invalid username or pasword'
+   context['form'] = LoginForm()
    return render(request, 'login.html', context=context)
 
 
